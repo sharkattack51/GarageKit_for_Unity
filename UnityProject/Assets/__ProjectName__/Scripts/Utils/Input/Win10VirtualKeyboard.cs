406bbf1129fbe971433049c7c25c203909229e33
sharkattack51@gmail.com <sharkattack51@gmail.com>
2019-10-29 17:57:54 +0900

    add Win10 TouchKeybord script

---
+++ UnityProject/Assets/__ProjectName__/Scripts/Utils/Input/Win10VirtualKeyboard.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace GarageKit
+{
+    public class Win10VirtualKeyboard : MonoBehaviour
+    {
+#if UNITY_STANDALONE_WIN
+        [DllImport("user32")]
+        static extern IntPtr FindWindow(String sClassName, String sAppName);
+
+        [DllImport("user32")]
+        static extern bool PostMessage(IntPtr hWnd, uint Msg, int wParam, int lParam);
+
+        private static Process _onScreenKeyboardProcess = null;
+
+
+        public static void ShowTouchKeyboard()
+        {
+            ExternalCall("C:\\Program Files\\Common Files\\Microsoft Shared\\ink\\tabtip.exe", null, false);
+        }
+
+        public static void HideTouchKeyboard()
+        {
+            uint WM_SYSCOMMAND = 274;
+            int SC_CLOSE = 61536;
+            IntPtr ptr = FindWindow("IPTip_Main_Window", null);
+            PostMessage(ptr, WM_SYSCOMMAND, SC_CLOSE, 0);
+        }
+
+        public static void ShowOnScreenKeyboard()
+        {
+            if(_onScreenKeyboardProcess == null || _onScreenKeyboardProcess.HasExited)
+                _onScreenKeyboardProcess = ExternalCall("OSK", null, false);
+        }
+
+        public static void HideOnScreenKeyboard()
+        {
+            if(_onScreenKeyboardProcess != null && !_onScreenKeyboardProcess.HasExited)
+                _onScreenKeyboardProcess.Kill();
+        }
+
+        public static void RepositionOnScreenKeyboard(Rect rect)
+        {
+            ExternalCall("REG", @"ADD HKCU\Software\Microsoft\Osk /v WindowLeft /t REG_DWORD /d " + (int)rect.x + " /f", true);
+            ExternalCall("REG", @"ADD HKCU\Software\Microsoft\Osk /v WindowTop /t REG_DWORD /d " + (int)rect.y + " /f", true);
+            ExternalCall("REG", @"ADD HKCU\Software\Microsoft\Osk /v WindowWidth /t REG_DWORD /d " + (int)rect.width + " /f", true);
+            ExternalCall("REG", @"ADD HKCU\Software\Microsoft\Osk /v WindowHeight /t REG_DWORD /d " + (int)rect.height + " /f", true);
+        }
+
+        private static Process ExternalCall(string filename, string arguments, bool hideWindow)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = filename;
+            startInfo.Arguments = arguments;
+
+            if(hideWindow)
+            {
+                startInfo.RedirectStandardOutput = true;
+                startInfo.RedirectStandardError = true;
+                startInfo.UseShellExecute = false;
+                startInfo.CreateNoWindow = true;
+            }
+
+            Process process = new Process();
+            process.StartInfo = startInfo;
+            process.Start();
+
+            return process;
+        }
+#endif
+
+        private GameObject currentSelectable;
+
+
+        void Awake()
+        {
+
+        }
+
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+#if UNITY_STANDALONE_WIN
+            if(EventSystem.current != null)
+            {
+                if(EventSystem.current.currentSelectedGameObject == null)
+                {
+                    currentSelectable = null;
+                    Win10VirtualKeyboard.HideTouchKeyboard();
+                }
+                else
+                {
+                    if(EventSystem.current.currentSelectedGameObject.Equals(currentSelectable))
+                        return;
+                    else
+                    {
+                        currentSelectable = EventSystem.current.currentSelectedGameObject;
+
+                        if(currentSelectable.GetComponent<InputField>() != null)
+                            Win10VirtualKeyboard.ShowTouchKeyboard();
+                        else
+                            Win10VirtualKeyboard.HideTouchKeyboard();
+                    }
+                }
+            }
+        }
+#endif
+    }
+}
---
+++ UnityProject/Assets/__ProjectName__/Scripts/Utils/UI/InputfieldZenkakuBSFix.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.UI;
+
+// 日本語入力の全角変換中に確定させない状態でInputFieldからフォーカスを外すと変換中の文字が倍加するバグがあり、倍加させない
+// original script
+// https://qiita.com/monolith8/items/a88b13ecc1121fa10450
+
+namespace GarageKit 
+{
+    [RequireComponent(typeof(InputField))]
+    public class InputfieldZenkakuBSFix : MonoBehaviour
+    {
+        private InputField uiInputField;
+
+        private string currentStr = "";
+        private int currentLength = 0;
+        private int selectinAnchorPos = 0;
+
+
+        void Awake()
+        {
+
+        }
+
+        void Start()
+        {
+            uiInputField = this.gameObject.GetComponent<InputField>();
+        } 
+        
+        void Update()
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                if(uiInputField.text != "")
+                {
+                    if(currentStr != uiInputField.text)
+                    {
+                        if(currentStr == "")
+                        {
+                            if(uiInputField.text.Length > 1 && uiInputField.text.Length % 2 == 0)
+                            {
+                                int halfLen = uiInputField.text.Length / 2;
+
+                                if(uiInputField.text.Substring(0, halfLen) == uiInputField.text.Substring(halfLen, halfLen))
+                                {
+                                    // 未確定時
+                                    uiInputField.text = uiInputField.text.Substring(0, halfLen);
+                                }
+                            }
+                        }
+                        else
+                        {
+                            if(uiInputField.text.Length - currentLength > 1)
+                            {
+                                if(currentLength + (selectinAnchorPos - currentLength) * 2 == uiInputField.text.Length)
+                                {
+                                    string ck = uiInputField.text.Substring(currentLength);
+
+                                    if(ck.Length > 1 && ck.Length % 2 == 0)
+                                    {
+                                        int halfLen = ck.Length / 2;
+                                        if(ck.Substring(0, halfLen) == ck.Substring(halfLen, halfLen))
+                                        {
+                                            // 未確定時
+                                            string subStr1 = uiInputField.text.Substring(0, currentLength);
+                                            string subStr2 = ck.Substring(0, halfLen);
+                                            uiInputField.text = subStr1 + subStr2;
+                                        }
+                                    }
+                                }
+                                else
+                                {
+                                    int back = (uiInputField.text.Length - (currentLength + (selectinAnchorPos - currentLength) * 2)) / 2;
+                                    int front = currentLength - back;
+                                    string ck = uiInputField.text.Remove(uiInputField.text.Length - back).Substring(front);
+
+                                    if(ck.Length > 1 && ck.Length % 2 == 0)
+                                    {
+                                        int halfLen = ck.Length / 2;
+                                        if(ck.Substring(0, halfLen) == ck.Substring(halfLen, halfLen))
+                                        {
+                                            string subStr1 = uiInputField.text.Substring(0, front);
+                                            string subStr2 = ck.Substring(0, halfLen);
+                                            string subStr3 = uiInputField.text.Substring(uiInputField.text.Length - back, back);
+                                            uiInputField.text = subStr1 + subStr2 + subStr3;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            currentStr = uiInputField.text;
+            currentLength = uiInputField.text.Length;
+            selectinAnchorPos = uiInputField.selectionAnchorPosition;
+        }
+
+        void LateUpdate()
+        {
+            // 他のinputfieldに変換中の文字が表示されるのを防ぐため選択中のみ
+            if(uiInputField.GetComponent<InputField>().isFocused == true)
+            {
+                // 強制的にラベルを即時更新します。キャレットと表示されている文字列の位置を再計算します。
+                uiInputField.ForceLabelUpdate();
+            }
+        }
+    }
+}
