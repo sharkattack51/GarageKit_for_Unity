7c0211977153fc59c5c0d5954bad3001b1d53baa
sharkattack51@gmail.com <sharkattack51@gmail.com>
2020-10-07 15:47:13 +0900

    added random salt option.

--- UnityProject/Assets/__ProjectName__/Scripts/Utils/Object/TextRandomizer.cs
+++ UnityProject/Assets/__ProjectName__/Scripts/Utils/Object/TextRandomizer.cs
@@ -3,78 +3,83 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace GarageKit
+[RequireComponent(typeof(Text))]
+public class TextRandomizer : MonoBehaviour
 {
-    [RequireComponent(typeof(Text))]
-    public class TextRandomizer : MonoBehaviour
+    public string randomChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!#$%&()-=^~|@[{;+:*]}<>?";
+
+    private Text uiText;
+
+
+    void Awake()
     {
-        public string randomChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!#$%&()-=^~|@[{;+:*]}<>?";
 
-        private Text uiText;
+    }
+
+    void Start()
+    {
+        uiText = this.gameObject.GetComponent<Text>();
+    }
+
+    void Update()
+    {
+
+    }
 
 
-        void Awake()
+    public void TextRandomizeIn(string goalText, float delay = 0.0f, int insertRndChrs = 10, bool defaultSalt = true)
+    {
+        StartCoroutine(RandomizeCoroutine(goalText, delay, insertRndChrs, defaultSalt));
+    }
+
+    private IEnumerator RandomizeCoroutine(string goalText, float delay, int insertRndChars, bool defaultSalt)
+    {
+        List<List<string>> charTable = new List<List<string>>();
+        for(int i = 0; i < goalText.Length; i++)
         {
-
-        }
-
-        void Start()
-        {
-            uiText = this.gameObject.GetComponent<Text>();
-        }
-
-        void Update()
-        {
-
-        }
-
-
-        public void TextRandomizeIn(string goalText, float delay = 0.0f, int insertRndChrs = 10)
-        {
-            StartCoroutine(RandomizeCoroutine(goalText, delay, insertRndChrs));
-        }
-
-        private IEnumerator RandomizeCoroutine(string goalText, float delay, int insertRndChars)
-        {
-            List<List<string>> charTable = new List<List<string>>();
-            for(int i = 0; i < goalText.Length; i++)
+            List<string> charSeq = new List<string>();
+            for(int j = 0; j <= i; j++)
+                charSeq.Add("");
+            if(defaultSalt)
             {
-                List<string> charSeq = new List<string>();
-                for(int j = 0; j <= i; j++)
-                    charSeq.Add("");
                 charSeq.Add("-");
                 charSeq.Add("-");
                 charSeq.Add("-");
                 charSeq.Add("+");
                 charSeq.Add("+");
-                for(int j = 0; j < insertRndChars; j++)
+            }
+            else
+            {
+                for(int j = 0; j < 5; j++)
                     charSeq.Add(GetRandomChar());
-                for(int j = 0; j < goalText.Length - i; j++)
-                    charSeq.Add(goalText[i].ToString());
-
-                charTable.Add(charSeq);
             }
+            for(int j = 0; j < insertRndChars; j++)
+                charSeq.Add(GetRandomChar());
+            for(int j = 0; j < goalText.Length - i; j++)
+                charSeq.Add(goalText[i].ToString());
 
-            uiText.text = "";
-            yield return new WaitForSeconds(delay);
-
-            int frame = 0;
-            int length = charTable[0].Count;
-            while(frame < length)
-            {
-                uiText.text = "";
-                for(int i = 0; i < goalText.Length; i++)
-                    uiText.text += charTable[i][frame];
-
-                yield return new WaitForSeconds(0.03f);
-
-                frame++;
-            }
+            charTable.Add(charSeq);
         }
 
-        private string GetRandomChar()
+        uiText.text = "";
+        yield return new WaitForSeconds(delay);
+
+        int frame = 0;
+        int length = charTable[0].Count;
+        while(frame < length)
         {
-            return randomChars[Random.Range(0, randomChars.Length)].ToString();
+            uiText.text = "";
+            for(int i = 0; i < goalText.Length; i++)
+                uiText.text += charTable[i][frame];
+
+            yield return new WaitForSeconds(0.03f);
+
+            frame++;
         }
+    }
+
+    private string GetRandomChar()
+    {
+        return randomChars[Random.Range(0, randomChars.Length)].ToString();
     }
 }
