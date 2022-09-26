1c6eed80c0ee9a7fb1e9d37a0af00503464c5158
sharkattack51@gmail.com <sharkattack51@gmail.com>
2022-09-26 14:07:42 +0900

    - feat: Added full file access permission request for Android 11+.

--- UnityProject/Assets/__ProjectName__/Scripts/Utils/AndroidUtil.cs
+++ UnityProject/Assets/__ProjectName__/Scripts/Utils/AndroidUtil.cs
@@ -164,5 +164,58 @@ namespace GarageKit
 #endif
         }
 #endregion
+
+        public static int GetApiLevel()
+        {
+            int apiLevel = 0;
+
+#if !UNITY_EDITOR && UNITY_ANDROID
+            using(AndroidJavaClass cVersion = new AndroidJavaClass("android.os.Build$VERSION"))
+            {
+                apiLevel = cVersion.GetStatic<int>("SDK_INT");
+            }
+#endif
+            return apiLevel;
+        }
+
+#region SCOPED STRAGE ACCESS
+        public static void RequestAllFilesAccessPermission()
+        {
+#if UNITY_ANDROID
+            Debug.LogWarning("add to AndroidManifest.xml: <uses-permission android:name=\"android.permission.MANAGE_EXTERNAL_STORAGE\" />");
+#endif
+
+#if !UNITY_EDITOR && UNITY_ANDROID
+            using(AndroidJavaClass cUnityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+            using(AndroidJavaObject oCurrentActivity = cUnityPlayer.GetStatic<AndroidJavaObject>("currentActivity"))
+            {
+                string packageName = oCurrentActivity.Call<string>("getPackageName");
+
+                if(GetApiLevel() >= 30) // Android11
+                {
+                    using(AndroidJavaClass cEnvironment = new AndroidJavaClass("android.os.Environment"))
+                    {
+                        bool isExternalStorageManager = cEnvironment.CallStatic<bool>("isExternalStorageManager");
+                        if(isExternalStorageManager)
+                            return;
+                    }
+
+                    using(AndroidJavaObject oIntent = new AndroidJavaObject("android.content.Intent", "android.settings.MANAGE_APP_ALL_FILES_ACCESS_PERMISSION"))
+                    using(AndroidJavaClass cUri = new AndroidJavaClass("android.net.Uri"))
+                    using(AndroidJavaObject uri = cUri.CallStatic<AndroidJavaObject>("parse", "package:" + packageName))
+                    {
+                        oIntent.Call<AndroidJavaObject>("addCategory", "android.intent.category.DEFAULT");
+                        oIntent.Call<AndroidJavaObject>("setData", uri);
+                        oCurrentActivity.Call("startActivity", oIntent);
+                    }
+
+                    // restart app
+                    Debug.LogWarning("restart app for set all files access permission.");
+                    Application.Quit();
+                }
+            }
+#endif
+        }
+#endregion
     }
 }
