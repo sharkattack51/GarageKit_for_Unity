a68b8f6ab29c51bfb89151e590b981b343c026b0
sharkattack51@gmail.com <sharkattack51@gmail.com>
2025-02-14 19:15:44 +0900

    feat: added UDP range send function. feat: added CancellationToken to each function argument to cancel UDP asynchronous processing.

--- UnityProject/Assets/__ProjectName__/Scripts/Utils/Network/UDPSender.cs
+++ UnityProject/Assets/__ProjectName__/Scripts/Utils/Network/UDPSender.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using UnityEngine;
 
 using Cysharp.Threading.Tasks;
@@ -31,14 +32,14 @@ namespace GarageKit
 
 
 #region Send
-        public void Send(string dataStr, string address = null, int? port = null)
+        public void Send(string dataStr, string address = null, int? port = null, CancellationToken ct = default(CancellationToken))
         {
             // byte配列に変換して送信
             byte[] data = Encoding.UTF8.GetBytes(dataStr);
-            Send(data, address, port);
+            Send(data, address, port, ct);
         }
 
-        public void Send(byte[] data, string address = null, int? port = null)
+        public void Send(byte[] data, string address = null, int? port = null, CancellationToken ct = default(CancellationToken))
         {
             if(!this.gameObject.activeSelf)
                 return;
@@ -49,6 +50,9 @@ namespace GarageKit
                 port = this.port;
 
             UniTask.RunOnThreadPool(() => {
+                if(ct.IsCancellationRequested)
+                    return;
+
                 try
                 {
                     Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
@@ -62,33 +66,107 @@ namespace GarageKit
                 {
                     Debug.LogWarning("UDPSender :: " + e.Message);
                 }
-            }).Forget();
+            }, cancellationToken: ct).Forget();
         }
 
-        public void TryContinuousSend(string dataStr, int tryCount, float span = 0.1f, string address = null, int? port = null)
+        public void TryContinuousSend(string dataStr, int tryCount, float span = 0.1f, string address = null, int? port = null, CancellationToken ct = default(CancellationToken))
         {
-            StartCoroutine(ContinuousSendCoroutine(dataStr, tryCount, span, address, port));
+            StartCoroutine(ContinuousSendCoroutine(dataStr, tryCount, span, address, port, ct));
         }
 
-        private IEnumerator ContinuousSendCoroutine(string dataStr, int tryCount, float span, string address, int? port)
+        private IEnumerator ContinuousSendCoroutine(string dataStr, int tryCount, float span, string address, int? port, CancellationToken ct = default(CancellationToken))
         {
             for(int i = 0; i < tryCount; i++)
             {
-                Send(dataStr, address, port);
+                if(ct.IsCancellationRequested)
+                    break;
+                else
+                    Send(dataStr, address, port);
+
                 yield return new WaitForSeconds(span);
             }
         }
 #endregion
 
+#region RangeSend
+        private List<string> MakeAddressRange(string startAddress, int addressNum)
+        {
+            List<string> addresses = new List<string>();
+
+            string[] ips = startAddress.Split(new string[]{ "." }, StringSplitOptions.None);
+            if(ips.Length != 4)
+            {
+                Debug.LogWarning("UDPSender :: address range error");
+                return addresses;
+            }
+
+            int[] ipNums = new int[4];
+            bool err = false;
+            for(int i = 0; i < 4; i++)
+            {
+                if(!int.TryParse(ips[i], out ipNums[i]))
+                {
+                    err = true;
+                    break;
+                }
+
+                if(ipNums[i] < 0 || ipNums[i] > 255)
+                {
+                    err = true;
+                    break;
+                }
+            }
+            if(err)
+            {
+                Debug.LogWarning("UDPSender :: make address range error");
+                return addresses;
+            }
+
+            for(int i = ipNums[3]; i < ipNums[3] + addressNum; i++)
+            {
+                if(i < 0 || i > 255)
+                {
+                    Debug.LogWarning("UDPSender :: make address range error");
+                    break;
+                }
+
+                addresses.Add(string.Format("{0}.{1}.{2}.{3}", ipNums[0], ipNums[1], ipNums[2], i));
+            }
+            return addresses;
+        }
+
+        public void RangeSend(string dataStr, List<string> addresses, int? port = null, CancellationToken ct = default(CancellationToken))
+        {
+            foreach(string address in addresses)
+                Send(dataStr, address, port, ct);
+        }
+
+        public void RangeSend(string dataStr, string startAddress, int addressNum, int? port = null, CancellationToken ct = default(CancellationToken))
+        {
+            RangeSend(dataStr, MakeAddressRange(startAddress, addressNum), port, ct);
+        }
+
+        public void TryContinuousRangeSend(string dataStr, List<string> addresses, int tryCount, float span = 0.1f, int? port = null, CancellationToken ct = default(CancellationToken))
+        {
+            foreach(string address in addresses)
+                TryContinuousSend(dataStr, tryCount, span, address, port, ct);
+        }
+
+        public void TryContinuousRangeSend(string dataStr, string startAddress, int addressNum, int tryCount, float span = 0.1f, int? port = null, CancellationToken ct = default(CancellationToken))
+        {
+            TryContinuousRangeSend(dataStr, MakeAddressRange(startAddress, addressNum), tryCount, span, port, ct);
+        }
+#endregion
+
 #region Broadcast
-        public void Broadcast(string dataStr, int? port = null)
+        public void Broadcast(string dataStr, int? port = null, CancellationToken ct = default(CancellationToken))
         {
             // byte配列に変換して送信
             byte[] data = Encoding.UTF8.GetBytes(dataStr);
-            Broadcast(data, port);
+            Broadcast(data, port, ct);
         }
 
-        public void Broadcast(byte[] data, int? port = null)
+        public void Broadcast(byte[] data, int? port = null, CancellationToken ct = default(CancellationToken))
         {
             if(!this.gameObject.activeSelf)
                 return;
@@ -97,6 +175,9 @@ namespace GarageKit
                 port = this.port;
 
             UniTask.RunOnThreadPool(() => {
+                if(ct.IsCancellationRequested)
+                    return;
+
                 try
                 {
                     Socket broadcastSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
@@ -111,19 +192,23 @@ namespace GarageKit
                 {
                     Debug.LogWarning("UDPSender :: " + e.Message);
                 }
-            }).Forget();
+            }, cancellationToken: ct).Forget();
         }
 
-        public void TryContinuousBroadcast(string dataStr, int tryCount, float span = 0.1f, int? port = null)
+        public void TryContinuousBroadcast(string dataStr, int tryCount, float span = 0.1f, int? port = null, CancellationToken ct = default(CancellationToken))
         {
-            StartCoroutine(ContinuousBroadcastCoroutine(dataStr, tryCount, span, port));
+            StartCoroutine(ContinuousBroadcastCoroutine(dataStr, tryCount, span, port, ct));
         }
 
-        private IEnumerator ContinuousBroadcastCoroutine(string dataStr, int tryCount, float span, int? port)
+        private IEnumerator ContinuousBroadcastCoroutine(string dataStr, int tryCount, float span, int? port, CancellationToken ct = default(CancellationToken))
         {
             for(int i = 0; i < tryCount; i++)
             {
-                Broadcast(dataStr, port);
+                if(ct.IsCancellationRequested)
+                    break;
+                else
+                    Broadcast(dataStr, port);
+
                 yield return new WaitForSeconds(span);
             }
         }
