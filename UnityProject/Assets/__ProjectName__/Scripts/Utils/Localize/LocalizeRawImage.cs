343c1621c7d1c4aa05708035376e52d0c2816733
sharkattack51@gmail.com <sharkattack51@gmail.com>
2026-01-17 12:26:28 +0900

    feat: added LocalizeRawImage

---
+++ UnityProject/Assets/__ProjectName__/Scripts/Utils/Localize/LocalizeRawImage.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GarageKit.Localize
+{
+    [ExecuteInEditMode]
+    public class LocalizeRawImage : MonoBehaviour, ILocalize
+    {
+        private static List<LocalizeRawImage> localizeList = new List<LocalizeRawImage>();
+        public static void LocalizeAll(LANGUAGE lang)
+        {
+            foreach(LocalizeRawImage rawImg in localizeList)
+                rawImg.Localize(lang);
+        }
+
+        public RawImage uiRawImage;
+        public List<Texture2D> localizeTexs;
+        public LANGUAGE lang;
+
+
+        void Awake()
+        {
+            localizeList.Add(this);
+
+            if(uiRawImage == null)
+                uiRawImage = this.gameObject.GetComponent<RawImage>();
+        }
+
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+            if(Application.isEditor)
+                Localize(this.lang);
+        }
+
+        void OnDestroy()
+        {
+            localizeList.Remove(this);
+        }
+
+
+        public void Localize(LANGUAGE lang)
+        {
+            this.lang = lang;
+
+            if(localizeTexs.Count == 1)
+                uiRawImage.texture = localizeTexs[0];
+            else if(localizeTexs.Count > (int)lang)
+                uiRawImage.texture = localizeTexs[(int)lang];
+        }
+    }
+}
